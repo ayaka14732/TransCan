ba18964c822c4fe1b02b2658720ef79a246c80a2
Ayaka Mikazuki <ayaka@mail.shn.hk>
2022-10-15 13:05:18 +0800

    Simplify wakong

--- lib/dataloader/tokenization_worker.py
+++ lib/dataloader/tokenization_worker.py
@@ -1,22 +1,29 @@
-import jax
+from typing import Callable
 import jax.numpy as np
 from transformers import BartTokenizer
+from wakong import Wakong
 
-from ..preprocessing.distort_sentence import distort_sentence
-from ..random.wrapper import KeyArray, split_key
+from ..random.wrapper import KeyArray, key2seed
+
+tokenizer = None
+
+def distort_sentence(wakong: Callable, sentence: str) -> str:
+    words = sentence.split(' ')  # TODO: possibility to use Blingfire?
+    masked_words = wakong(words, mask_token='<mask>')
+    return ' '.join(masked_words)
 
 def tokenization_worker(x) -> np.ndarray:
-    jax.config.update('jax_platforms', 'cpu')  # enforce CPU in subprocesses
-
     # sentences: list[str], key: KeyArray
     sentences, key = x
 
+    seed = key2seed(key)  # TODO: optimisation: avoid seed conversion on every function call
+    wakong = Wakong(seed=seed)
+
     global tokenizer
-    if 'tokenizer' not in globals():
+    if tokenizer is None:
         tokenizer = BartTokenizer.from_pretrained('facebook/bart-base')
 
-    keys = split_key(key, num=len(sentences))
-    distorted_sentences = [distort_sentence(sentence, key=key) for sentence, key in zip(sentences, keys)]
+    distorted_sentences = [distort_sentence(wakong, sentence) for sentence in sentences]
 
     x = tokenizer(sentences, return_tensors='np', max_length=256, padding='max_length', truncation=True, add_prefix_space=True)
     y = tokenizer(distorted_sentences, return_tensors='np', max_length=256, padding='max_length', truncation=True, add_prefix_space=True)
